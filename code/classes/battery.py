c0da0f814d58761a350ca1a846d28b6cbfe08b00
Wim <44021592+MyBunzor@users.noreply.github.com>
2018-11-08 12:09:17 +0100

    Testcommit

---
+++ code/classes/battery.py
@@ -0,0 +1,10 @@
+class Battery(object):
+
+    """Defines the battery class."""
+    def __init__(self, xvalue, yvalue, output):
+        self.xvalue = xvalue
+        self.yvalue = yvalue
+        self.capacity = capacity
+
+
+        super(, self).__init__()
---
+++ code/classes/house.py
@@ -0,0 +1,9 @@
+class House(object):
+
+    """Defines the house class."""
+    def __init__(self, xvalue, yvalue, output):
+            self.xvalue = xvalue
+            self.yvalue = yvalue
+            self.output = output
+
+        super(, self).__init__()
